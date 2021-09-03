9fb1fe44fcca98f8ac4ba92bea7f05295c938526
chanhakim <chanhakim17@gmail.com>
2021-09-03 01:14:30 -0700

    feat: fix navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lawry-a2';
+
+  toggleMenu() {
+    var x = document.getElementById("menu-rows")!;
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  closeMenu() {
+    var x = document.getElementById("menu-rows")!;
+    x.style.display = "none";
+  }
 }
